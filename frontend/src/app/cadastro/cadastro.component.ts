ac8032cd8bcaf0bc6d073ea6dbd5ad11cee8c061
Stefany <scampanhoni@gmail.com>
2022-10-13 11:59:28 -0300

    começando as validations

--- frontend/src/app/cadastro/cadastro.component.ts
+++ frontend/src/app/cadastro/cadastro.component.ts
@@ -4,34 +4,45 @@ import { AuthService } from './auth.service';
 @Component({
   selector: 'app-cadastro',
   templateUrl: './cadastro.component.html',
-  styleUrls: ['./cadastro.component.css']
+  styleUrls: ['./cadastro.component.css'],
 })
 export class CadastroComponent implements OnInit {
+  constructor(private authService: AuthService) {}
 
-  constructor(private authService:AuthService) { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-    
-    // Example starter JavaScript for disabling form submissions if there are invalid fields
-(() => {
-  'use strict'
+  enviar() {
+    this.validacao();
 
-  // Fetch all the forms we want to apply custom Bootstrap validation styles to
-  const forms = document.querySelectorAll('.needs-validation')
-
-  // Loop over them and prevent submission
-  Array.from(forms).forEach((form:any) => {
-    form.addEventListener('submit', (event:any) => {
-      if (!form.checkValidity()) {
-        event.preventDefault()
-        event.stopPropagation()
-      }
-
-      form.classList.add('was-validated')
-    }, false)
-  })
-})()
   }
 
-}
+  private email = document.getElementById('validationEmail');
 
+  validacao() {
+    // Example starter JavaScript for disabling form submissions if there are invalid fields
+    (() => {
+      'use strict';
+
+      // Fetch all the forms we want to apply custom Bootstrap validation styles to
+      const forms = document.querySelectorAll('.needs-validation');
+      
+      // Loop over them and prevent submission
+      Array.from(forms).forEach((form: any) => {
+        form.addEventListener(
+          'submit',
+          (event: any) => {
+            if (!form.checkValidity()) {
+              event.preventDefault();
+              event.stopPropagation();
+            }
+
+            form.classList.add('was-validated');
+          },
+          false
+        );
+      });
+    })();
+
+
+  }
+}
