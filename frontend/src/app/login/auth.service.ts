4f86d363b242e02d76ec7cd922252e96a464a692
Stefany <scampanhoni@gmail.com>
2022-10-19 10:07:38 -0300

    login e autenticação completa

--- frontend/src/app/login/auth.service.ts
+++ frontend/src/app/login/auth.service.ts
@@ -1,55 +1,52 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+} from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class AuthService implements CanActivate{
+export class AuthService implements CanActivate {
+  constructor(public http: HttpClient, public router: Router) {}
 
-  constructor(
-    public http:HttpClient,
-    public router:Router
-  ) { }
-
-  public login(user:any) {
+  public login(user: any) {
     const http_options: any = {
       Headers: new HttpHeaders({
-        'Access-Control-Allow-Origin': '*'
+        'Access-Control-Allow-Origin': '*',
       }),
     };
-
-    let formData = new FormData();
-    formData.append('login',user.email)
-    formData.append('senha',user.senha);
 
     return this.http.post(
       'http://localhost:8080/auth',
       {
-        login :user.email,
-        senha :user.senha,
+        email: user.email,
+        senha: user.senha,
       },
       http_options
     );
-  }  
+  }
 
-  autenticar(){
-    localStorage.setItem('is_auth','true');
+  autenticar() {
+    localStorage.setItem('is_auth', 'true');
   }
 
   canActivate(
-    route: ActivatedRouteSnapshot, 
-    state: RouterStateSnapshot): boolean | Observable<boolean> | Promise<boolean> {
-    
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot,
+
+  ): boolean | Observable<boolean> | Promise<boolean> {
     let is_auth = JSON.parse(String(localStorage.getItem('is_auth')));
     console.log(is_auth);
-    if (is_auth){
-      this.router.navigate(['/dashboard']);
+    if (is_auth) {
       return true;
-    }else{
+    } else {
       this.router.navigate(['/login']);
       return false;
     }
-  }  
+  }
 }
